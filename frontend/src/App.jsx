5a30f02265a1d88e185cc4c2487c95215b50da00
Sabira-R <123287816+Sabira-R@users.noreply.github.com>
2025-01-03 01:18:50 +0600

    breadcrumb navigation done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
 import ResponsiveNavbar from './components/Navbar'
 import AllRoutes from './routes/AllRoutes'
+import Breadcrumb from './components/Breadcrumb'
 
 function App() {
-
-
   return (
-
     <div className='bg-[#e1dae4]'>
       <ResponsiveNavbar />
+      <Breadcrumb />
       <AllRoutes />
     </div>
-
   )
 }
 
